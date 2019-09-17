0d6e3bccc25744a27e814fbee71dfb9bfec4e15e
leonardo <elleoleonvalencia@gmail.com>
2019-09-17 10:50:31 -0400

    terminada la consulta avanzada, falta ponerle la opcion de cambiar el tipo de grafico

--- src/components/orgJibacoaMan/index.js
+++ src/components/orgJibacoaMan/index.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import SymAgricUrbanaPoint from './graficoGeneral';
+import GraficoGeneral from './graficoGeneral';
 import TablaEspecifica from './tablaEspecifica';
+
+import Dropdown from '../dropdown/index';
 
 class orgLasCuevas extends Component {
 
-	
+	state = {
+		measures: ["SymAgricUrbanaPoint.count"],
+		dimensions: ["SymAgricUrbanaPoint.tecnologia"]
+	};
+
+	camposMeasures = async measures => {
+		await this.setState({ measures: measures })
+		console.log(measures);
+	}
+
+	camposDimensions = async dimensions => {
+		await this.setState({ dimensions: dimensions })
+		console.log(dimensions);
+	}
 
 	render() {
 		return (
 			<div>
-                <SymAgricUrbanaPoint />
-                <TablaEspecifica />
+				<Dropdown camposMeasures={this.camposMeasures} camposDimensions={this.camposDimensions} />
+				<GraficoGeneral camposMeasures={this.state.measures} camposDimensions={this.state.dimensions} />
+				<TablaEspecifica />
 			</div>
 		);
 	}
