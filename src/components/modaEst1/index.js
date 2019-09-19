fcc7042c8c8b89f55e754f1ca3c21ae670978c05
hany93 <hany93cuba@gmail.com>
2019-09-19 14:51:04 -0400

    cambios est1

---
+++ src/components/modaEst1/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { Modal, Row, Col } from 'antd';
+import GraficoGeneral from './graficoGeneral';
+import est1 from '../../images/est1.jpg'
+import Card1 from '../card';
+
+class modalEst1 extends Component {
+    state = {
+        visible: false
+    };
+
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    };
+
+    handleCancel = e => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <Card1 title='Estadística 1' descripcion='En este tema se pueden realizar análisis estadísticos.' fotos={est1} show={this.showModal} />
+                <Modal
+                    title="Estadística 1"
+                    visible={this.state.visible}
+                    onCancel={this.handleCancel}
+                    footer={null}
+                    centered
+                    width={'98%'}
+                >
+                    <div>
+                        <Row>
+                            <br />
+                        </Row>
+                        <Row>
+                            <GraficoGeneral municipio={this.props.match.params.municipio} />
+                        </Row>
+                    </div>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default withRouter(modalEst1);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
 import defaultComponent from '../components/defaultComponent/index';
 // import orgLasCuevasMan from '../components/orgLasCuevasMan/index';
-import orgJibacoaMan from '../components/orgJibacoaMan/index';
+//import orgJibacoaMan from '../components/orgJibacoaMan/index';
 // import orgLaHerraduraMan from '../components/orgLaHerraduraMan/index';
 // import orgLosCanarreosMan from '../components/orgLosCanarreosMan/index';
 
 const Router = () => (
     <BrowserRouter>
         <Switch>
+            <Route path="/:municipio" component={defaultComponent} />
             {/* <Route exact path="/" component={defaultComponent} />
-            <Route path="/Las Cuevas" component={orgLasCuevasMan} />
-            
+            <Route path="/Las Cuevas" component={orgLasCuevasMan} />            
             <Route path="/La Herradura" component={orgLaHerraduraMan} />
             <Route path="/Los Canarreos" component={orgLosCanarreosMan} /> */}
-            <Route path="/:municipio" component={defaultComponent} />
-
-            <Route path="/Jibacoa" component={orgJibacoaMan} />
-
+            {/*<Route path="/Jibacoa" component={orgJibacoaMan} />*/}
         </Switch>
     </BrowserRouter>
 );
